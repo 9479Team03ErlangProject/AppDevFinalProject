e9057fb91c03cba3304ba04536f395ae45c5b257
Kevin Roy Maglaqui <22334101@slu.edu.ph>
2024-12-09 20:51:04 +0800

    user server added

--- src/user_server.erl
+++ src/user_server.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%%% @author Kevin Roy Maglaqui
+%%% @author Maglaqui Kevin Roy
 %%% @copyright (C) 2024, <COMPANY>
 %%% @doc
 %%%  This module provides a simple in-memory user authentication system
@@ -10,32 +10,31 @@
 %%%-------------------------------------------------------------------
 
 -module(user_server).
--author("Maglaqui Kevin Roy").
+-author("Maglaqui Kevin").
 -export([start/0, register_user/2, authenticate_user/2, stop/0]).
 
 
+% Starts the user server by creating an ETS table named 'users'.
 start() ->
   ets:new(users, [named_table, public]),
   {ok, "User server started"}.
 
-
+% Stops the user server by deleting the ETS table named 'users'.
 stop() ->
   ets:delete(users),
-
   {ok, "User server stopped"}.
 
-
+% Registers a user with a given Username and Password.
 register_user(Username, Password) ->
   case ets:lookup(users, Username) of
     [] ->
       ets:insert(users, {Username, Password}),
       {ok, "User registered"};
-
     _ ->
       {error, "User already exists"}
   end.
 
-
+% Authenticates a user by checking the given Username and Password.
 authenticate_user(Username, Password) ->
   case ets:lookup(users, Username) of
     [{_, StoredPassword}] when StoredPassword == Password ->
